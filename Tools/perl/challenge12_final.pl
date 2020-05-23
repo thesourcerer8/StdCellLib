0d214d6075f3a141c49d454b848e0a483c6b554e
nikhil2798 <34730166+nikhil2798@users.noreply.github.com>
2020-05-23 21:32:38 +0530

    Extracted log file & printed the frequency in Ghz

---
+++ Tools/perl/challenge12_final.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+use strict;
+    
+    our $data;
+    my $file = "sta_log.txt"; 
+    open(FH, $file) or die("File $file not found"); 
+      
+    while(my $String = <FH>) 
+    { 
+        
+	if($String =~ m/Computed maximum clock frequency (.*?)Hz/) 
+        { 
+	        our $var = $1;				#temp.
+		our ($fin) = $var =~ m/(\d+\.\d+)/;     #magnitude
+		print "The computed maximum clock frequency is $var Hz\n"; 
+		our $order = substr($var, -1);          #exponential power
+		
+		if ($order eq "K")
+		{
+			my $opt = $fin * 1e-6;
+			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+		}
+		if ($order eq "M")
+		{
+			my $opt = $fin * 1e-3;
+			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+		}
+		if ($order eq "G")
+		{
+			my $opt = $fin;
+			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+		}
+		if ($order eq "T")
+		{
+			my $opt = $fin * 1e3;
+			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+		}
+        } 
+	
+	else 
+	{
+		print "Computed maximum clock frequency isn't avaiable in the report.\n";	
+	}
+    }
+
+
