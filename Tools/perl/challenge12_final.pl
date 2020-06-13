d4673c0600b1d753deb77eeb77935c314ea266d6
nikhil2798 <34730166+nikhil2798@users.noreply.github.com>
2020-06-13 17:29:15 +0530

    Update challenge12_final.pl

--- Tools/perl/challenge12_final.pl
+++ Tools/perl/challenge12_final.pl
@@ -1,46 +1,63 @@
 #!/usr/bin/perl -w
 use strict;
     
-    our $data;
-    my $file = "sta_log.txt"; 
-    open(FH, $file) or die("File $file not found"); 
-      
-    while(my $String = <FH>) 
-    { 
-        
-	if($String =~ m/Computed maximum clock frequency (.*?)Hz/) 
-        { 
-	        our $var = $1;				#temp.
-		our ($fin) = $var =~ m/(\d+\.\d+)/;     #magnitude
-		print "The computed maximum clock frequency is $var Hz\n"; 
-		our $order = substr($var, -1);          #exponential power
-		
-		if ($order eq "K")
-		{
-			my $opt = $fin * 1e-6;
-			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
-		}
-		if ($order eq "M")
-		{
-			my $opt = $fin * 1e-3;
-			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
-		}
-		if ($order eq "G")
-		{
-			my $opt = $fin;
-			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
-		}
-		if ($order eq "T")
-		{
-			my $opt = $fin * 1e3;
-			print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
-		}
-        } 
-	
-	else 
-	{
-		print "Computed maximum clock frequency isn't avaiable in the report.\n";	
-	}
-    }
-    close(FH);
+    
+our $data;
+print "Please enter the file name:\n";
+my $file = <>; 
+chomp $file;
 
+	if ($file ne "sta_log.txt") 
+		{
+			print "File not found.\n";
+		}
+
+		else
+		{
+
+    			open(FH, $file) or die("File $file not found"); 
+      
+    			while(my $String = <FH>) 
+    				{ 
+        
+					if($String =~ m/Computed maximum clock frequency (.*?)Hz/) 
+        					{ 
+	        					our $var = $1;												    #temp.
+							our ($fin) = $var =~ m/(\d+\.\d+)/;     						#magnitude
+							print "The computed maximum clock frequency is $var Hz\n"; 
+							our $order = substr($var, -1);          						#exponential power
+		
+							if ($order eq "K")
+								{
+									my $opt = $fin * 1e-6;
+									print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+								}
+		
+							if ($order eq "M")
+								{
+									my $opt = $fin * 1e-3;
+									print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+								}
+		
+							if ($order eq "G")
+								{
+									my $opt = $fin;
+									print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+								}
+							
+							if ($order eq "T")
+								{
+									my $opt = $fin * 1e3;
+									print ("The computed maximum clock frequency in giga hertz is $opt G Hz\n");
+								}
+        					} 
+	
+					else 
+						{
+							print "Computed maximum clock frequency isn't avaiable in the report.\n";	
+						}
+    				}
+    
+    			close(FH);
+			
+		}
