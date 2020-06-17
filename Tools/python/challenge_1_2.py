b3dd4ba6c7e81fe58293911f7b0692f3a851f8e3
Shivani Shah <shivani.shah269@gmail.com>
2020-06-17 12:52:17 +0530

    update challenge1_2

--- Tools/python/challenge_1_2.py
+++ Tools/python/challenge_1_2.py
@@ -2,14 +2,24 @@
 # find function will find that line in the report and will print frequency in MHz and GHz.
 
 from sys import argv
+import math
 
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
+    
 def find(txt):
     for line in txt.readlines():
         if 'frequency' in line:
-            frequency_MHz = line.split()
-            print ("\nMaximum frequency in MHz is: ",frequency_MHz[7],"MHz")
-            frequency = float("".join(frequency_MHz[7]))
-            print ("\nMaximum frequency in GHz is: ",frequency/(1000),"GHz")
+            frequency = line.split()
+            freq = float(frequency[7])
+            if (frequency[8] == 'MHz'):
+                print ("\nMaximum frequency in", frequency[8] ,"is: ",frequency[7],frequency[8])
+                print ("\nMaximum frequency in GHz is: ",round_down(freq/1000,7),"GHz")
+            if (frequency[8] == 'KHz'):
+                print ("\nMaximum frequency in", frequency[8] ,"is: ",frequency[7],frequency[8])
+                print ("\nMaximum frequency in MHz is: ",round_down(freq/1000,7),"MHz")
+                print ("\nMaximum frequency in GHz is: ",round_down(freq/1000000,9),"GHz")
             break   
             
     if 'frequency' not in line:        
