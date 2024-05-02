76d19180b1b7de90035ed1d3de1ae25aeea5ef3b
Philipp Gühring <pg@futureware.at>
2024-05-03 01:42:36 +0200

    Added some documentation

--- Tools/perl/truthtable.pl
+++ Tools/perl/truthtable.pl
@@ -333,9 +333,9 @@ EOF
 	my @a=();
 	foreach(@ins)
 	{
-          push @a,$res{$_}?"$_":"(!$_)"; # Here we are collecting all values for a AO representation, e.g. (A && !B && C) || (!A && B && C))
+          push @a,$res{$_}?"$_":"(!$_)"; # Here we are collecting all values for a AO representation, e.g. (A && !B && C) || (!A && B && C))  "Sum-of-Product"
 	}
-	push @{$results{$out}{$res{$out}}},join($format eq "liberty"?"&":" && ",@a); # Here the single values are put together: (A && !B && C)
+	push @{$results{$out}{$res{$out}}},join($format eq "liberty"?"&":" && ",@a); # Here the single values are put together: (A && !B && C)   "Sum-of-Product"
       }
 
       if($format eq "text")
