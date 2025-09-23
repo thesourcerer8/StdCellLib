4a3d14e359c1866eae8489360b8a6d6038f803b8
Philipp Gühring <pg@futureware.at>
2025-09-23 15:25:49 +0200

    Slowing down the updates

--- Tools/reporter.sh
+++ Tools/reporter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo This script generates a build report every 10 seconds, so that you can monitor the progress of the standard cell generation.
+echo This script generates a build report every 30 seconds, so that you can monitor the progress of the standard cell generation.
 rm -f .done
 perl ../Tools/perl/buildreport.pl 
 firefox buildreport.html &
 while [ ! -f .done ] ;
 do
 	perl ../Tools/perl/buildreport.pl 
-	sleep 10
+	sleep 30
 done
 
